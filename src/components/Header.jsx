77b49b69c5cb6973a6cde987233f19b3d51bd0a1
hansaja-gunasekara <hgmgunasekara@gmail.com>
2023-12-21 22:36:55 +0530

    update: Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../Assets/Logo.svg";
 
 const Header = () => {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const handleSelection = () => {
+    setIsSelected(!isSelected);
+    console.log(isSelected);
+  };
+
   return (
     <header>
-      <nav className="bg-primary-color border-gray-200 px-4 lg:px-6 py-2.5">
-        <div className="flex flex-wrap items-center justify-between lg:mx-14 md:mx-9 sm:mx-4">
+      <nav className="bg-primary-color border-gray-200 px-4 lg:px-6 py-2.5 h-[77px] flex flex-col justify-center relative">
+        <div className="flex items-center justify-between lg:mx-14 md:mx-9 sm:mx-4">
           <a className="flex items-center">
             <img
               src={Logo}
@@ -13,80 +20,72 @@ const Header = () => {
               alt=""
             />
           </a>
-          <div className="flex items-center">
+          <div className="flex items-center justify-end w-full md:order-2 md:w-0">
             <button
-              data-collapse-toggle="mobile-menu-2"
               type="button"
-              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-white"
               aria-controls="mobile-menu-2"
               aria-expanded="false"
+              onClick={handleSelection}
             >
               <span className="sr-only">Open main menu</span>
               <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
+                width="31"
+                height="25"
+                viewBox="0 0 31 25"
+                fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <svg
-                className="hidden w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
+                <rect y="0.5" width="31" height="4" rx="2" fill="white" />
+                <rect y="10.5" width="31" height="4" rx="2" fill="white" />
+                <rect y="20.5" width="31" height="4" rx="2" fill="white" />
               </svg>
             </button>
           </div>
-          <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-            id="mobile-menu-2"
-          >
-            <ul className="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-white rounded bg-primary-700 md:bg-transparent md:text-primary-700 md:p-0"
-                  aria-current="page"
-                >
-                  SERVICES
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0"
-                >
-                  ABOUT US
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0"
-                >
-                  CONTACT US
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0"
-                >
-                  CAREERS
-                </a>
-              </li>
-            </ul>
+          <div className="md:w-full md:flex md:justify-end">
+            <div
+              className={
+                "items-center justify-between w-full md:flex md:w-auto md:order-1 absolute md:static z-50 bg-primary-color top-full right-0 left-0 " +
+                (isSelected ? "block" : "hidden")
+              }
+              id="mobile-menu-2"
+            >
+              <ul className="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 pl-3 pr-4 text-white border-gray-100 bg-primary-700 md:border-0 border-y md:bg-transparent md:text-primary-700 md:p-0"
+                    aria-current="page"
+                  >
+                    SERVICES
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 pl-3 pr-4 text-white border-b border-gray-100 md:border-0 md:hover:text-primary-700 md:p-0"
+                  >
+                    ABOUT US
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 pl-3 pr-4 text-white border-b border-gray-100 md:border-0 md:hover:text-primary-700 md:p-0"
+                  >
+                    CONTACT US
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 pl-3 pr-4 text-white border-b border-gray-100 md:border-0 md:hover:text-primary-700 md:p-0"
+                  >
+                    CAREERS
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
